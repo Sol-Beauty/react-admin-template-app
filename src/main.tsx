a13676993821fec561e0ee15aa10a2f67857038c
Paul Garcia <paul.garcia@solbeautyandcare.com>
2025-02-05 16:06:15 -0800

    feat: config project tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./styles/index.css";
+
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import Root from "./root.tsx";
+
+const root = document.getElementById("root");
+
+createRoot(root!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Root />} />
+    </Routes>
+  </BrowserRouter>,
+);
