0f56b65df7c67c7a7580ef60dc043bca52526feb
Paul Garcia <paul.garcia@solbeautyandcare.com>
2025-02-07 15:58:05 -0800

    feat: add .env example file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMeta {
+  env: {
+    /** The URL of the main API to be used */
+    VITE_BASE_API_URL: string;
+    /** The URL of the auth API to be used */
+    VITE_AUTH_API_URL: string;
+    /** Activate the bypass protection if a custom login will be used  */
+    VITE_BYPASS_PROTECTION: "true" | "false";
+  };
+}
